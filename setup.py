1322ee9e3d85439ca35a226a5b68a081c1fb920e
Joel Collins <joel@jtcollins.net>
2020-09-07 15:10:41 +0100

    Updated doc description

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"""A pure Python interface for the Raspberry Pi camera module."""
+"""A pure Python interface for the Raspberry Pi camera module, with extra features and fixes."""
 
 import os
 import sys
